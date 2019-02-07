2cc706a98416e3e7313c73ed6d18759186226f0f
Brian De Maio <bdemaio1@gmail.com>
2019-02-06 19:07:52 -0500

    Getting data from jsonplaceholder api

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class ResourceList extends Component {
+  state = { resources: [] };
+
+  componentDidMount() {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ resources: res.data });
+      })
+      .catch(err => {
+        console.log("Error of it all " + err);
+      });
+  }
+
   render() {
-    return <div>{this.props.resource}</div>;
+    return <div>{this.state.resources.length}</div>;
   }
 }
 
