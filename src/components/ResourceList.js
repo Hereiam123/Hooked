b2f24f5f17b0605cd8195fa80d623aba94fc09f4
Brian De Maio <bdemaio1@gmail.com>
2019-02-06 19:28:58 -0500

    Refactored, now using useEffect to use axios api call

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,37 +1,17 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-class ResourceList extends Component {
-  state = { resources: [] };
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
 
-  componentDidMount() {
-    axios
-      .get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-      .then(res => {
-        this.setState({ resources: res.data });
-      })
-      .catch(err => {
-        console.log("Error of it all " + err);
-      });
-  }
+  const fetchResource = async () => {
+    const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/${resource}`
+    );
+    setResources(res.data);
+  };
 
-  async componentDidUpdate(prevProps) {
-    console.log(prevProps);
-    if (prevProps.resource !== this.props.resource) {
-      axios
-        .get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-        .then(res => {
-          this.setState({ resources: res.data });
-        })
-        .catch(err => {
-          console.log("Error of it all " + err);
-        });
-    }
-  }
-
-  render() {
-    return <div>{this.state.resources.length}</div>;
-  }
-}
+  return <div>{resources.length}</div>;
+};
 
 export default ResourceList;
