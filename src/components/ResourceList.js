195820231b1619ed239013a4f6dd0a651596be86
Brian De Maio <bdemaio1@gmail.com>
2019-02-06 19:46:55 -0500

    Removed functionality out into another method

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
   const [resources, setResources] = useState([]);
 
   const fetchResource = async resource => {
@@ -18,6 +18,11 @@ const ResourceList = ({ resource }) => {
     [resource]
   );
 
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
   return (
     <ul>
       {resources.map(record => (
