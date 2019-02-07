26e6a14ff2cf0304a955b95fa6a68dcbc46ef0b5
Brian De Maio <bdemaio1@gmail.com>
2019-02-06 19:30:20 -0500

    API call working

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async () => {
+  const fetchResource = async resource => {
     const res = await axios.get(
       `https://jsonplaceholder.typicode.com/${resource}`
     );
     setResources(res.data);
   };
+
+  useEffect(() => {
+    fetchResource(resource);
+  }, []);
 
   return <div>{resources.length}</div>;
 };
